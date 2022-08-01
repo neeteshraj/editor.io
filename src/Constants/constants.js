413c1c41fedea2df734c989dcd5a56395398af58
Nitesh Raj Khanal <nitesh.khanal19@gmail.com>
2022-08-01 20:05:00 +0545

    refactor: :art: constant values seperated
    Constants values used to editors are seperated into another directory

---
+++ src/Constants/constants.js
@@ -0,0 +1,14 @@
+export const InitialVal = `## Welcome to Editor
+
+**Features**
+
+- _Custom Toolbar_
+- _Html Support_
+- _Live Preview_
+- _Download File_`;
+
+export const htmlDefault = `<h2>Hello User</h2>`;
+
+export const cssDefault = `body{
+    text-align:center;
+}`;
