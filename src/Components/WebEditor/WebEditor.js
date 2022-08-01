ed54d193e51fb3fda6d07f1272a3f9deb5f6ff7f
Nitesh Raj Khanal <nitesh.khanal19@gmail.com>
2022-08-01 11:17:53 +0545

    feat: Web Editor Complete

--- src/Components/WebEditor/WebEditor.js
+++ src/Components/WebEditor/WebEditor.js
@@ -1,37 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import Editor from "../Editor/Editor";
 import Footer from "../Footer/Footer";
+import { useLocalStorage } from "../../Hooks/LocalStorage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../App.css";
 
-const WebEditor = () => {
-  const nogutterValue = true;
+function LanguageManager() {
+  const getBlobURL = (code, type) => {
+    const blob = new Blob([code], { type });
+    return URL.createObjectURL(blob);
+  };
+
+  const htmlDefault = `<h2>Hello User</h2>`;
+
+  const cssDefault = `body{
+  text-align:center;
+}`;
+
+  const [htmlVal, updateHtmlStorage] = useLocalStorage("html", htmlDefault);
+  const [cssVal, updateCssStorage] = useLocalStorage("css", cssDefault);
+  const [jsVal, updateJsStorage] = useLocalStorage("js", "");
+
+  const [html, updateHtml] = useState(htmlVal);
+  const [css, updateCss] = useState(cssVal);
+  const [js, updateJs] = useState(jsVal);
+
+  const cssURL = getBlobURL(css, "text/css");
+  const jsURL = getBlobURL(js, "text/javascript");
+
+  const srcDoc = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+      </head>
+        <body>${html}
+        ${js && `<script src="${jsURL}"></script>`}
+        </body>
+      </html>`;
+
+  useEffect(() => {
+    setTimeout(() => {}, 500);
+    updateHtmlStorage(html);
+    updateCssStorage(css);
+    updateJsStorage(js);
+  }, [html, css, js, updateHtmlStorage, updateCssStorage, updateJsStorage]);
+
   return (
     <div>
       <Container fluid={true} className="pane pane-top">
-        <Row nogutters={nogutterValue.toString()}>
+        <Row noGutters={true}>
           <Col md={4} className="editor-lang">
             <div className="editor-text">
               <i className="fab fa-html5"> </i> Html
             </div>
+            <Editor
+              language="xml"
+              value={html}
+              onChange={(newVal) => {
+                updateHtml(newVal);
+              }}
+            />
           </Col>
 
           <Col md={4} className="editor-lang">
             <div className="editor-text">
               <i className="fab fa-css3-alt"></i> Css
             </div>
+            <Editor
+              language="css"
+              value={css}
+              onChange={(newVal) => {
+                updateCss(newVal);
+              }}
+            />
           </Col>
 
           <Col md={4} className="editor-lang">
             <div className="editor-text">
               <i className="fab fa-js-square"></i> Js
             </div>
+            <Editor
+              language="javascript"
+              value={js}
+              onChange={(newVal) => {
+                updateJs(newVal);
+              }}
+            />
           </Col>
         </Row>
       </Container>
 
       <Container fluid={true} className="pane pane-bottom">
-        <Row nogutters={nogutterValue.toString()}>
+        <Row noGutters={true}>
           <iframe
-            srcDoc="<html><head></head><body></body></html>"
+            srcDoc={srcDoc}
             className="output-pane"
             allowFullScreen
             title="output"
@@ -42,6 +108,6 @@ const WebEditor = () => {
       <Footer />
     </div>
   );
-};
+}
 
-export default WebEditor;
+export default LanguageManager;
